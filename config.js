f6861238f421fff9653c1f80251f68263bd8c71e
Alex Gordon <gordon112524@gmail.com>
2021-05-30 18:10:11 -0500

    Removed config from gitignore

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://adminAlex:passwordAlex@cluster0.kiigj.mongodb.net/homie?retryWrites=true&w=majority",
+  SECRET_CODE: "homie secret code mane",
+};
